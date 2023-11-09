9514b7411db710d46807f76ecac9ea75130b71b2
Oce <oceane_000@hotmail.fr>
2023-11-09 11:26:53 +0100

    front ping pong

--- src/pages/chans/Chat2.jsx
+++ src/pages/chans/Chat2.jsx
@@ -1,89 +1,16 @@
+// Import des bibliothèques nécessaires
 import React, { useState, useEffect } from "react";
 import { Grid, Container, TextField, Button } from "@mui/material";
-import "../assets/css/Chat.css";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import socketIO from "socket.io-client";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
-const socket = socketIO.connect("http://localhost:3000");
 
-const onConnect = () => {
-  console.log("Connected socket");
-  console.log(socket.id);
-};
 
-socket.on("connect", onConnect);
-
-socket.on("message", function (data) {
-  console.log("message", data);
-});
-
-const Chat = () => {
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("");
-
-  const [nom, setNom] = useState("");
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const nomParam = searchParams.get("nom");
-    console.log(nomParam);
-
-    if (nomParam) {
-      setNom(nomParam);
-    }
-
-    socket.on("message", function (data) {
-      const [senderNom, receivedMessage] = data;
-      setMessages([
-        ...messages,
-        { text: receivedMessage, sender: { nom: senderNom } },
-      ]);
-    });
-  }, [messages]);
-
-  const BtnDeco = () => {
-    socket.on("disconnect", () => {
-      console.log(socket.id); // undefined
-    });
-    alert("Deconnected");
-    navigate("/");
-  };
-
-  const handleSendMessage = () => {
-    if (message) {
-      /*
-      setMessages([...messages, { text: message, sender: { nom } }]);*/
-      setMessage("");
-
-      socket.emit("message", [nom, message]);
-    }
-  };
-
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      handleSendMessage();
-    }
-  };
-
-  return (
-    <Container
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        width: "100%",
-        padding: 0,
-      }}
-    >
-      <Container
-        style={{ width: "30%", height: "100vh", backgroundColor: "red" }}
-      >
-        <h1>Channel</h1>
-      </Container>
-      <Container className="chat-container" style={{ widht: "70%" }}>
+const ChatContent2 = ({ messages, handleSendMessage, message, handleKeyPress, BtnDeco, nom }) => {
+    return (
+      <Container className="chat-container" style={{ width: '70%' }}>
         <div
           style={{
             width: "100%",
@@ -103,7 +30,7 @@ const Chat = () => {
           >
             Se deco
           </button>
-
+  
           <h2>User {nom}</h2>
         </div>
         <hr />
@@ -118,7 +45,7 @@ const Chat = () => {
               <div
                 key={index}
                 className={`message ${
-                  msg.sender.nom === "user1" ? "user1" : "user2"
+                  msg.sender === "user" ? "user-message" : "other-message"
                 }`}
               >
                 {msg.text}
@@ -157,8 +84,7 @@ const Chat = () => {
           </Grid>
         </Grid>
       </Container>
-    </Container>
-  );
-};
+    );
+  };
 
-export default Chat;
+  export default ChatContent2;
---
+++ src/pages/tempo.jsx
@@ -0,0 +1,6 @@
+/*
+ <Container style={{display:'flex',flexDirection:'row',width:'100%'}}>
+      <Container style={{width:'20%',backgroundColor:'white '}}>
+<h1>Raler</h1>
+      </Container>
+    </Container>*/
